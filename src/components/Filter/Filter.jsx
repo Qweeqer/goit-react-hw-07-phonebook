a9d6f4f5f4082c2b2e8104edafedd31b4bdf56be
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-01 21:26:06 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,23 @@
 import PropTypes from 'prop-types';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { filteredContacts, getFilter } from '../../redux/filterSlice';
 import './Filter.module.css';
 
-export const Filter = ({ onFilter }) => (
-  <label>
-    <input
-      type="text"
-      name="filter"
-      onChange={onFilter}
-      placeholder="Find contacts by name or number"
-    />
-  </label>
-);
+export const Filter = () => {
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+  return (
+    <label>
+      <input
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={e => dispatch(filteredContacts(e.target.value))}
+        placeholder="Find contacts by name or phone"
+      />
+    </label>
+  );
+};
 
 Filter.propTypes = {
   onFilter: PropTypes.func,
