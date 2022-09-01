7c377fef2fa5c7c123755cd4933234da6ac7f1d7
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-01 21:26:21 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,58 +1,78 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import {
+  useFetchContactsQuery,
+  useAddContactMutation,
+} from '../../redux/contactsAPI';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import PropTypes from 'prop-types';
 import './ContactForm.module.css';
 
-const INITIAL_FORM_STATE = {
-  name: '',
-  number: '',
-};
+let schema = yup.object().shape({
+  name: yup.string().required(),
+  phone: yup.number().required(),
+});
 
-export function ContactForm({ onSubmit }) {
-  const [formData, setFormData] = useState(INITIAL_FORM_STATE);
-
-  const handleChange = ({ target: { name, value } }) => {
-    setFormData(prevState => ({ ...prevState, [name]: value }));
+export const ContactForm = () => {
+  const initialValues = {
+    name: '',
+    phone: '',
   };
+  const [addContact] = useAddContactMutation();
+  const { data: contacts } = useFetchContactsQuery();
 
-  const handleSubmit = event => {
-    event.preventDefault();
-
-    const newContact = {
-      name: formData.name,
-      number: formData.number,
-    };
-    onSubmit(newContact);
-    setFormData({ number: '', name: '' });
+  const handleSubmit = (values, { resetForm }) => {
+    // event.preventDefault();
+    const { name, phone } = values;
+    const oldСontact = contacts.find(
+      contact =>
+        (contact.name.toLowerCase() === name.toLowerCase() &&
+          contact.phone === phone) ||
+        contact.phone === phone
+    );
+    if (oldСontact) {
+      return alert(`This phone ${phone} is already in contacts`);
+    }
+    console.log('contacts', contacts);
+    addContact({ name, phone });
+    // onSubmit(addContact);
+    resetForm();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        placeholder="Name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
-      <input
-        type="tel"
-        name="number"
-        value={formData.number}
-        placeholder="Number"
-        onChange={handleChange}
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +. For example +380670000000"
-        required
-      />
-      <button type="submit">Add contact</button>
-    </form>
+    <Formik
+      initialvalues={initialValues}
+      validationschema={schema}
+      onSubmit={handleSubmit}
+    >
+      <form>
+        <input
+          type="text"
+          name="name"
+          // value={contact.name}
+          // onChange={handleChange}
+          placeholder="Name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <input
+          type="tel"
+          name="phone"
+          // value={contacts.phone}
+          // onChange={handleChange}
+          placeholder="Phone"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone phone must be digits and can contain spaces, dashes, parentheses and can start with +. For example +380670000000"
+          required
+        />
+        <button type="submit">Add contact</button>
+      </form>
+    </Formik>
   );
-}
+};
 // }
 
 ContactForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func,
 };
